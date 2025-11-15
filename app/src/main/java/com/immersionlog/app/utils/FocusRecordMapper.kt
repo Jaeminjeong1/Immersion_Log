072ca3b9209f1d555b91be2c0718e2f4f7d59079
Jaeminjeong1 <82368069+Jaeminjeong1@users.noreply.github.com>
2025-11-15 16:23:35 +0900

    feat(utils): FocusRecord <-> FocusRecordEntity 매핑 함수 추가

---
+++ app/src/main/java/com/immersionlog/app/utils/FocusRecordMapper.kt
@@ -0,0 +1,27 @@
+package com.immersionlog.app.utils
+
+
+import com.immersionlog.app.domain.entity.FocusRecord
+import com.immersionlog.app.domain.entity.FocusRecordEntity
+
+fun FocusRecord.toEntity(): FocusRecordEntity {
+    return FocusRecordEntity(
+        id = this.id,
+        date = this.date,
+        score = this.score,
+        minutes = this.minutes,
+        category = this.category,
+        memo = this.memo
+    )
+}
+
+fun FocusRecordEntity.toDomain(): FocusRecord {
+    return FocusRecord(
+        id = this.id,
+        date = this.date,
+        score = this.score,
+        minutes = this.minutes,
+        category = this.category,
+        memo = this.memo
+    )
+}
